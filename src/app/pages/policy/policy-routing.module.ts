e8c37574030009cd49357814e182c55ab1654c40
Ramon Rodelo <ramon.rodeloc@gmail.com>
2023-03-11 08:18:52 -0700

    se mueve de carpeta archivo tailwind.conf.js

--- src/app/pages/policy/policy-routing.module.ts
+++ src/app/pages/policy/policy-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PolicyComponent } from './policy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PolicyComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PolicyRoutingModule { }
+export class PolicyRoutingModule {}
